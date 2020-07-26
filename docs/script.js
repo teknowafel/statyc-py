f4bc47fd7bc22fa33f5f186270a607b4c283f4bc
Purple <rmdu45@gmail.com>
2020-07-26 21:50:00 +0200

    v2.0

--- docs/script.js
+++ docs/script.js
@@ -14,8 +14,10 @@
   });
   $(".article .readMoreButton").on("click", function () {
     let ARTICLE = $(this).attr("id").replace("btnMore", "");
-    let STATUSES = { true: false, false: true };
-    let STATUS = STATUSES[$("#" + ARTICLE + "-content").attr("data-open")];
-    $("#" + ARTICLE + "-content").attr("data-open", STATUS);
-    $('html, body').animate({scrollTop: ($("#" + ARTICLE).offset().top)}, 700);});
+    let STATUSES = { true: [false, "", "Read Less"], false: [true, "60pt", "Read More"] };
+    $('html, body').animate({ scrollTop: ($("#" + ARTICLE).offset().top) }, 700);
+    $("#" + ARTICLE + "-content").attr("data-open", STATUSES[$("#" + ARTICLE + "-content").attr("data-open")][0]);
+    $("#" + ARTICLE + "-content").attr("style", "max-height:" + $("#" + ARTICLE + "-content").prop('scrollHeight') + "pt");
+    $("#" + $(this).attr("id")).html( STATUSES[$("#" + ARTICLE + "-content").attr("data-open")][2]);
+  });
 })();
