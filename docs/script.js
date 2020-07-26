8b662a987b4497ea0f9ab036d580a5473ac16e40
Purple <rmdu45@gmail.com>
2020-07-26 19:53:51 +0200

    update

--- docs/script.js
+++ docs/script.js
@@ -1,59 +1,21 @@
-const copyToClipboard = str => {
-  const el = document.createElement('textarea');
-  el.value = str;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
-  document.body.appendChild(el);
-  const selected =
-    document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-  if (selected) {
-    document.getSelection().removeAllRanges();
-    document.getSelection().addRange(selected);
-  }
 
-};
-
-function copiedAlert(id) {
-  document.getElementById(id).textContent = 'Copied!';
-  setTimeout(function (){
-    document.getElementById(id).textContent = 'Share';
-  }, 1000);
-}
-
-function readMore(name, articleName, button) {
-  var elmnt = document.getElementById(articleName);
-  var h = document.getElementById(name).scrollHeight;
-  document.getElementById(name).style.maxHeight = h + 'pt';
-  document.getElementById(button).textContent = 'Read Less';
-  
-  setTimeout(function (){
-    elmnt.scrollIntoView({ behavior: 'smooth' });
-  }, 700);
-}
-
-function readLess(name, articleName, button) {
-  document.getElementById(name).style.maxHeight = '60pt';
-  var elmnt = document.getElementById(articleName);
-  document.getElementById(button).textContent = 'Read More';
-
-  setTimeout(function (){
-    elmnt.scrollIntoView({ behavior: 'smooth' });
-  }, 700);
-}
-
-function readButton(name, articleName, button) {
-  var article = document.getElementById(name);
-  var articleOpen = article.getAttribute('data-open');
-
-  if (articleOpen == "false"){
-    readMore(name, articleName, button)
-    article.setAttribute("data-open", 'true')
-  } else {
-    readLess(name, articleName, button)
-    article.setAttribute("data-open", 'false')
-  }
-}
+(function () {
+  $(".article .copyButton").on("click", function () {
+    let ID = $(this).attr("id");
+    let SELECTED = document.createElement('textarea');
+    SELECTED.value = 'https://teknowafel.github.io/statyc-py/index.html#' + ID.replace("btn", "");
+    document.body.appendChild(SELECTED);
+    SELECTED.select();
+    document.execCommand('copy');
+    document.body.removeChild(SELECTED);
+    window.getSelection().removeAllRanges();
+    $("#" + ID).html('Copied!');
+    setTimeout(function () { $("#" + ID).html('Share'); }, 1000);
+  });
+  $(".article .readMoreButton").on("click", function () {
+    let ARTICLE = $(this).attr("id").replace("btnMore", "");
+    let STATUSES = { true: false, false: true };
+    let STATUS = STATUSES[$("#" + ARTICLE + "-content").attr("data-open")];
+    $("#" + ARTICLE + "-content").attr("data-open", STATUS);
+    $('html, body').animate({scrollTop: ($("#" + ARTICLE).offset().top)}, 700);});
+})();
