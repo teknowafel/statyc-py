a6b611c8a9c9520260f6e8b2edee795729db32ac
Purple <rmdu45@gmail.com>
2020-07-26 23:09:38 +0200

    final update

--- docs/script.js
+++ docs/script.js
@@ -1,23 +1,47 @@
 
 (function () {
+
+//ON CLICK ON "SHARE"
+
   $(".article .copyButton").on("click", function () {
+
+    //DEFINE TEMPORARY VARIABLES
     let ID = $(this).attr("id");
     let SELECTED = document.createElement('textarea');
+
+    //SET THE TEXT GENERATED
     SELECTED.value = 'https://teknowafel.github.io/statyc-py/index.html#' + ID.replace("btn", "");
+
+    //COPY THE TEXT
     document.body.appendChild(SELECTED);
     SELECTED.select();
     document.execCommand('copy');
     document.body.removeChild(SELECTED);
     window.getSelection().removeAllRanges();
+
+    //TELL THE USER THAT THE TEXT IS COPIED & THEN RESET THE TEXT AFTER 1000ms
     $("#" + ID).html('Copied!');
     setTimeout(function () { $("#" + ID).html('Share'); }, 1000);
   });
+
+//ON CLICK ON "READ MORE"
+
   $(".article .readMoreButton").on("click", function () {
+
+    //DEFINE TEMPORARY VARIABLES
     let ARTICLE = $(this).attr("id").replace("btnMore", "");
-    let STATUSES = { true: [false, "", "Read Less"], false: [true, "60pt", "Read More"] };
+    let STATUSES = { true: [false, "Read Less"], false: [true, "Read More"] };
+
+    //SCROLL TO TOP
     $('html, body').animate({ scrollTop: ($("#" + ARTICLE).offset().top) }, 700);
+
+    //DEFINE DATA-OPEN OF CURRENT ID
     $("#" + ARTICLE + "-content").attr("data-open", STATUSES[$("#" + ARTICLE + "-content").attr("data-open")][0]);
+
+    //DEFINE THE MAX HEIGHT OF THE CURRENT ID (DISABLED BY CSS IF FALSE)
     $("#" + ARTICLE + "-content").attr("style", "max-height:" + $("#" + ARTICLE + "-content").prop('scrollHeight') + "pt");
-    $("#" + $(this).attr("id")).html( STATUSES[$("#" + ARTICLE + "-content").attr("data-open")][2]);
+
+    //UPDATE THE TEXT OF THE BUTTON
+    $("#" + $(this).attr("id")).html( STATUSES[$("#" + ARTICLE + "-content").attr("data-open")][1]);
   });
 })();
