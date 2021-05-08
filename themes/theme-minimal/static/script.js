b31551e35b6da4a369952379c2e7cddd58477ef9
teknowafel <39500700+teknowafel@users.noreply.github.com>
2021-05-08 14:23:05 -0400

    New theme! theme-minimal

---
+++ themes/theme-minimal/static/script.js
@@ -0,0 +1,67 @@
+//GETS BASE URL OF SITE
+const baseURL = window.location.origin+window.location.pathname;
+
+(function () {
+
+  //SEARCH THING! HANDLES SEARCH BOX
+  $("#searchbox").keyup(function() {
+    //Gets the value in the search box
+    var searchedText = $("#searchbox").val();
+    $("p:contains('"+searchedText+"')").each( function( i, element ) {
+      var content = $(element).text();
+      //Adds the highlighting
+      content = content.replace( searchedText, '<span class="search-found">' + searchedText + '</span>' );
+      $(element).html( content );
+    });
+
+  });
+
+//ON CLICK ON SCOLLER
+  $(".scroller button").on("click", function () {
+    $("html, body").animate({scrollTop : 0},500);
+  });
+
+//ON CLICK ON "SHARE"
+
+  $(".article .copyButton").on("click", function () {
+
+    //DEFINE TEMPORARY VARIABLES
+    let ID = $(this).attr("id");
+    let SELECTED = document.createElement('textarea');
+
+    //SET THE TEXT GENERATED
+    SELECTED.value = baseURL + '#' + ID.replace("btn", "");
+
+    //COPY THE TEXT
+    document.body.appendChild(SELECTED);
+    SELECTED.select();
+    document.execCommand('copy');
+    document.body.removeChild(SELECTED);
+    window.getSelection().removeAllRanges();
+
+    //TELL THE USER THAT THE TEXT IS COPIED & THEN RESET THE TEXT AFTER 1000ms
+    $("#" + ID).html('Copied!');
+    setTimeout(function () { $("#" + ID).html('Share'); }, 1000);
+  });
+
+//ON CLICK ON "READ MORE"
+
+  $(".article .readMoreButton").on("click", function () {
+
+    //DEFINE TEMPORARY VARIABLES
+    let ARTICLE = $(this).attr("id").replace("btnMore", "");
+    let STATUSES = { true: [false, "Read Less"], false: [true, "Read More"] };
+
+    //SCROLL TO TOP
+    $('html, body').animate({ scrollTop: ($("#" + ARTICLE).offset().top) }, 700);
+
+    //DEFINE DATA-OPEN OF CURRENT ID
+    $("#" + ARTICLE + "-content").attr("data-open", STATUSES[$("#" + ARTICLE + "-content").attr("data-open")][0]);
+
+    //DEFINE THE MAX HEIGHT OF THE CURRENT ID (DISABLED BY CSS IF FALSE)
+    $("#" + ARTICLE + "-content").attr("style", "max-height:" + $("#" + ARTICLE + "-content").prop('scrollHeight') + "pt");
+
+    //UPDATE THE TEXT OF THE BUTTON
+    $("#" + $(this).attr("id")).html( STATUSES[$("#" + ARTICLE + "-content").attr("data-open")][1]);
+  });
+})();
